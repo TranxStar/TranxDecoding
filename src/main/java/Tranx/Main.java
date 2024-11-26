19392dae522b2dffdf046dca07a047d8dee51c7f
TranxStar <tranxstar@163.com>
2024-11-26 23:44:08 +0800

    TXCD

--- src/main/java/Tranx/Main.java
+++ src/main/java/Tranx/Main.java
@@ -2,15 +2,17 @@ package Tranx;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.logging.Logger;
+//dialog无法弹出1111111111111111
 
-public class Main {
+public class  Main {
     public static final String NAME = "Tranx-DeCoding";
     public static final String VERSION = "v1.0";
     public static final String OWNER = "ShadowTranX-Team";
-
-
     public static Logger logger = Logger.getLogger("TDC");
+
     public static void main(String[] args) {
 
         logger.info(NAME + " " + VERSION + " 由 " + OWNER + " 驱动 ");
@@ -34,7 +36,7 @@ public class Main {
         //make window
         JFrame TD = new JFrame(NAME + " " + VERSION);
         //set icon
-        ImageIcon icon=new ImageIcon("/resources/icon.png");  //xxx?????????·????2.png??????????
+        ImageIcon icon=new ImageIcon("src/main/java/resources/icon.png");  //xxx?????????·????2.png??????????
         TD.setIconImage(icon.getImage());
         // ????????С
         TD.setSize((int) width, (int) height);
@@ -45,15 +47,29 @@ public class Main {
         lpath.setBounds(50,50,190,30);
         JLabel lchose = new JLabel("请选择：");
         lchose.setBounds(iwidth/2,20,60,30);
+        JLabel lwarning = new JLabel("无效的指向路径");
+        //filechooser
+        JFileChooser fc = new JFileChooser();
 
         //button
         JButton fmb = new JButton("文件转mp4");
         fmb.setBounds(iwidth/4-60,200,120,50);
         JButton mfb = new JButton("mp4转文件");
-        mfb.setBounds((iwidth/4)*3-60,iheight/2 +50,120,50);
+        mfb.setBounds((iwidth/4)*3-60,200,120,50);
+        JButton enter = new JButton();
+        enter.setBounds(380,50,18,20);
+        //button appearance
+        fmb.setBackground(Color.GRAY);
+        mfb.setBackground(Color.GRAY);
+        enter.setBackground(Color.GRAY);
+        Icon  et = new ImageIcon("src/main/java/resources/f.jpg");
+        enter.setIcon(et);
         //text1
         JTextField jtf1 = new JTextField();
         jtf1.setBounds(250,50,100,30);
+        //dialog
+        JDialog jd = new JDialog(TD,"warning",true);
+        jd.add(lwarning);
 
         //???
         //ImageIcon image = new ImageIcon(icon[i]);
@@ -65,6 +81,7 @@ public class Main {
 
         TD.add(fmb);
         TD.add(mfb);
+        TD.add(enter);
 
         //?????????С
         TD.setResizable(false);
@@ -76,6 +93,29 @@ public class Main {
 
 
         TD.setLocationRelativeTo(null);
+
+        enter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int resl = fc.showOpenDialog(null);
+
+                if (resl == JFileChooser.APPROVE_OPTION) {
+                    String luj = fc.getSelectedFile().getAbsolutePath();
+                    System.out.println(luj);
+                    jtf1.setText(luj);
+                }
+            }
+        });
+        mfb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (jtf1.getText() == "hhh"){
+                    jd.setVisible(true);
+
+                }
+            }
+        });
+
 
 
 
